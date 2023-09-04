96ada30cf0e96ea8a24f1d4ef1de487830309583
James Aguilar <799564+jaguilar@users.noreply.github.com>
2023-09-04 11:50:35 -0400

    Rework calibration, install server code.

--- main.py
+++ main.py
@@ -16,124 +16,209 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pybricks.tools import wait
+from pybricks.messaging import BluetoothMailboxServer, Mailbox
+from micropython import const
+import ustruct
+from typing import *
 import _thread
 
 brick = EV3Brick()
 calibrating = False
 
+turntable = Motor(Port.A)
+arm1 = Motor(Port.B, gears=[8, 40], positive_direction=Direction.COUNTERCLOCKWISE)
+arm2 = Motor(Port.C, gears=[[8, 36], [12, 36]], positive_direction=Direction.CLOCKWISE)
+turntable_limit_switch = TouchSensor(Port.S1)
 
-class MyScalingMotor:
-    def __init__(self, motor: Motor, scale: float):
-        self.motor = motor
-        self._scale = float(scale)
+turntable_range = [0, 0]
+turntable_min_speed = 15
+arm1_range = [0, 0]
+arm1_min_speed = int(15 / (40 / 8))
+arm2_range = [0, 0]
+arm2_min_speed = int(100 / ((36.0 / 8.0) * (36 / 12)))
 
-
-    def angle(self):
-        return self.motor.angle() / self._scale
-
-
-    def run(self, speed):
-        return self.motor.run(self._scale * speed)
-
-
-    def run_angle(self, speed, angle, **kwargs):
-        return self.motor.run_angle(self._scale*speed, self._scale*angle, **kwargs)
-
-
-    def run_target(self, speed, target, **kwargs):
-        print(self._scale * speed, self._scale * target, kwargs)
-        return self.motor.run_target(self._scale * speed, self._scale * target, **kwargs)
-
-
-    def reset_angle(self, new_angle):
-        return self.motor.reset_angle(new_angle * self._scale)
-
-
-    def stop(self):
-        return self.motor.stop()
-
-
-turn = Motor(Port.B)
-arm1 = MyScalingMotor(Motor(Port.C), scale=(40.0/12))
-arm2 = MyScalingMotor(Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE), scale=(20.0/12))
-
-
-class EWMA:
-    def __init__(self, start, alpha):
-        self._avg = start
-        self._alpha = alpha
-
-    def record(self, data):
-        self._avg = self._avg * (1.0-self._alpha) + self._alpha * data
-
-    def avg(self):
-        return self._avg
+# Geometry notes:
+#
+# Modelling in a right handed coordinate system:
+# * X increases to the right.
+# * Y increases going up.
+# * Z increases "toward" you, or to the right of X.
+#
+# All measurements are with the model facing to the right.
+#
+# Turntable rotation: 180 degrees.
+# Turntable 0: half way between limits.
+# Turntable positive direction: clockwise looking from above,
+# i.e. +rotation causes arm to point toward increasing Z.
+#
+# Arm 1 height: 12
+# Arm 1 offset from turntable: +2 in the direction of the arm.
+# Arm 1 length: 20
+# Arm 2 length: 15
+#
+# Arm rotation measurements: 0 degrees = pointing straight up.
+# Increasing arm rotation rotates clockwise.
+#
+# Arm 1 min: 17 degrees
+# Arm 1 max: 90 degrees (approx horizontal, artificial limit, not calibrated at the moment)
+# Arm 2 min: -6 degrees (can bend slightly further than straight up)
+# Arm 2 max: 161 degrees
+#
+# Calibrated degrees will not match the exact number of degrees, so to get
+# correct movement we have to scale all motor movements accordingly.
 
 
-def calibrate_throw(motor: Motor, speed: float, torque_fraction: int = 40):
-    motor.stop()
-    before_limits = motor.motor.control.limits()
-    motor.motor.control.limits(before_limits[0], before_limits[1], torque_fraction)
+def calibrate_arm1_raise():
+    arm1.run_until_stalled(-3 * arm1_min_speed, then=Stop.COAST, duty_limit=20)
+    arm1.reset_angle(73)
+    arm1_range[0] = 0
+    arm1_range[1] = 73
 
-    target_ms_per_degree = abs(1000.0 / speed)
-    stalled_ms_per_degree = target_ms_per_degree * 2
-    quit_ms_per_degree = max(100, target_ms_per_degree * 3)
-    avg_ms_per_degree = EWMA(target_ms_per_degree, 0.2)
 
-    motor.run(speed)
-    wait(100)
-    angle_before = motor.angle()
+def calibrate_arm2():
+    # Runs while arm1 is pointing up, so it should be safe to test our whole limit.
+    arm2.run_until_stalled(-3 * arm2_min_speed, duty_limit=30)
+    arm2.reset_angle(-6)
+    arm2_range[0] = -6
+    arm2.run_until_stalled(3 * arm2_min_speed, duty_limit=30)
+    arm2_range[1] = arm2.angle()
 
-    watch = StopWatch()
-    watch.resume()
+
+def calibrate_turntable():
+    turntable_limits = turntable.control.limits()
+    turntable.stop()
+    turntable.control.limits(turntable_limits[0], turntable_limits[1], 60)
+    turntable.run(turntable_min_speed)
+
+    while not turntable_limit_switch.pressed():
+        wait(10)
+
+    turntable.brake()
+    turntable.reset_angle(90)
+    turntable_range[1] = 90
+
+    turntable.stop()
+    turntable.control.limits(*turntable_limits)
+
+    turntable.run_until_stalled(-3 * turntable_min_speed, duty_limit=40)
+    turntable_range[0] = turntable.angle()
+
+
+def send_motor(
+    m: Motor, min_speed: int, range: Tuple[int, int], target: int, time_ms: int
+):
+    """Tells a motor to go to the given target position at a target time.
+
+    Each motor has an associated min_speed and range. The min_speed is
+    the *slowest* the motor is allowed to move. This is because some EV3
+    motors, especially the medium motor, have erratic behavior at lower speeds.
+    The range is the calibrated minimum and maximum angle value for the motor.
+    Requests for targets outside the range will instead be set to the range limit
+    instead.
+    """
+    actual_target = max(range[0], min(range[1], target))
+    ideal_speed = 1000 * float(abs(actual_target - m.angle())) / time_ms
+    actual_speed = max(ideal_speed, min_speed)
+    m.run_target(actual_speed, actual_target, wait=False)
+
+
+def send_turntable(target: int, time_ms: int):
+    send_motor(turntable, turntable_min_speed, turntable_range, target, time_ms)
+
+
+def send_arm1(target: int, time_ms: int):
+    send_motor(arm1, arm1_min_speed, arm1_range, target, time_ms)
+
+
+def send_arm2(target: int, time_ms: int):
+    send_motor(arm2, arm2_min_speed, arm2_range, target, time_ms)
+
+
+calibrate_arm1_raise()
+send_arm1(arm1_range[0] + 10, 2000)
+calibrate_arm2()
+send_arm2(arm2_range[0] + 20, 2000)
+calibrate_turntable()
+send_turntable(0, 2000)
+
+print(turntable_min_speed, arm1_min_speed, arm2_min_speed)
+print(turntable_range, arm1_range, arm2_range)
+
+
+send_turntable(45, 500)
+print("turntable_sent")
+send_arm1(50, 500)
+print("arm1_sent")
+send_arm2(10, 500)
+print("arm2_sent")
+
+wait(5000)
+
+print(turntable.angle(), arm1.angle(), arm2.angle())
+
+
+server = BluetoothMailboxServer()
+print('waiting for connection')
+server.wait_for_connection(1)
+print('connected')
+
+
+def update_positions():
+    """Thread for periodically updating the current_position mailbox."""
+    position_mailbox = Mailbox("current_position", server)
+
+    _UPDATE_FREQ_MS = const(33)
+    _FORMAT = "!hhh"
+
+    buffer = ustruct.pack(_FORMAT, turntable.angle(), arm1.angle(), arm2.angle())
+
     while True:
-        wait(50)
-        angle_after = motor.angle()
-        if angle_after != angle_before:
-            diff = 1.0 * abs(angle_after - angle_before)
-            ms_per_angle = watch.time() / diff
-            for x in range(int(diff)):
-                avg_ms_per_degree.record(ms_per_angle)
-            watch.reset()
-
-        print(watch.time())
-        if watch.time() > quit_ms_per_degree:
-            print('had to quit took too long to get degree: ', watch.time(), ' > ', quit_ms_per_degree)
-            break
-        if avg_ms_per_degree.avg() > stalled_ms_per_degree:
-            print('had to quit average fell too low: ', avg_ms_per_degree.avg(), ' > ', stalled_ms_per_degree)
-            break
-
-        angle_before = angle_after
-
-    motor.stop()
-    motor.motor.control.limits(*before_limits)
+        position_mailbox.send(buffer)
+        wait(_UPDATE_FREQ_MS)
+        ustruct.pack_into(_FORMAT, buffer, 0, turntable.angle(), arm1.angle(), arm2.angle())
 
 
-arm1_calib_speed = 10
-arm1_throw = 90
-arm2_calib_speed = 40
-arm2_throw = 60
 
-# For a given input degree, we have to set
+def send_ranges():
+    range_mailbox = Mailbox("calibrated_ranges", server)
 
-# Arm 2 has a 8:1 reduction. A speed of 50 seems precise enough.
-print(calibrate_throw(arm2, arm2_calib_speed, torque_fraction=100))
-arm2.run_angle(5, -10, then=Stop.HOLD, wait=True)
-arm2.reset_angle(-30)  # We start at -30 degrees from straight up.
+    buffer = ustruct.pack(
+        "!hhhhhh",
+        turntable_range[0],
+        turntable_range[1],
+        arm1_range[0],
+        arm1_range[1],
+        arm2_range[0],
+        arm2_range[1],
+    )
+    range_mailbox.send(buffer)
 
-# Arm 1 has a 36:20 reduction. This is about a 2:1 ratio and thus we'll reduce the speed by a factor of 4.
-print(calibrate_throw(arm1, arm1_calib_speed))
-arm1.reset_angle(0)  # We start pointing straight up.
 
-arm1.run_target(20, -45, then=Stop.HOLD, wait=True)
-arm1.run_target(20, -90, then=Stop.HOLD, wait=True)
-arm1.run_target(20, -22.5, then=Stop.HOLD, wait=True)
-arm1.run_target(20, -45-22.5, then=Stop.HOLD, wait=True)
-arm2.run_target(20, -60, then=Stop.HOLD, wait=True)
-arm2.run_target(20, -45, then=Stop.HOLD, wait=True)
-arm2.run_target(20, -90, then=Stop.HOLD, wait=True)
-arm2.run_target(20, -75, then=Stop.HOLD, wait=True)
+def receive_position_commands():
+    mailbox = Mailbox("target_position", server)
 
-arm1.stop()
-arm2.stop()
+    _FORMAT = "!hhhh"  # time_to_target, turntable, arm1, arm2
+
+    while True:
+        mailbox.wait_new()
+        (
+            time_to_target,
+            turntable_target,
+            arm1_target,
+            arm2_target,
+        ) = ustruct.unpack_from(_FORMAT, mailbox.read())
+        print('Got new target: in ', time_to_target, 'ms: ', turntable_target, arm1_target, arm2_target)
+
+        send_turntable(turntable_target, time_to_target)
+        send_arm1(arm1_target, time_to_target)
+        send_arm2(arm2_target, time_to_target)
+
+
+_thread.start_new_thread(receive_position_commands)
+_thread.start_new_thread(update_positions)
+
+send_ranges()
+
+print('main thread sleeping forever')
+while True:
+    wait(5000)
